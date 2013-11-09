8f382a2abccaf64a9f39c72250db60d59823f9ab
krzysiuk12 <kkrzysiu@poczta.onet.pl>
2013-11-09 02:11:19 +0100

    [Common] IDaoFactory, IServiceLocator

---
+++ SpringMvc/Models/Common/Interfaces/IDaoFactory.cs
@@ -0,0 +1,16 @@
+﻿using SpringMvc.Models.UserAccounts.Dao.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpringMvc.Models.Common.Interfaces
+{
+    public interface IDaoFactory
+    {
+        public IAuthorizationDao AuthorizationDao { get; set; }
+        public IAccountAdministrationDao AccountAdministrationDao { get; set; }
+        public ILogEventsDao LogEventsDao { get; set; }
+        public IUserInformationDao UserInformationDao { get; set; }
+    }
+}
---
+++ SpringMvc/Models/Common/Interfaces/IServiceLocator.cs
@@ -0,0 +1,16 @@
+﻿using SpringMvc.Models.UserAccounts.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpringMvc.Models.Common.Interfaces
+{
+    public interface IServiceLocator
+    {
+        public IAuthorizationService AuthorizationService { get; set; }
+        public IAccountAdministrationService AccountAdministrationService { get; set; }
+        public ILogEventsService LogEventsService { get; set; }
+        public IUserInformationService UserInformationService { get; set; }
+    }
+}
