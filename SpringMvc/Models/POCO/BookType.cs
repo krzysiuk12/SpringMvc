ca76ef4b34264f00a154f9846507ce9d865b3386
krzysiuk12 <kkrzysiu@poczta.onet.pl>
2013-11-12 20:58:30 +0100

    [Storehouse] BookType and BookTypeDetails

---
+++ SpringMvc/Models/POCO/BookType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpringMvc.Models.POCO
+{
+    public class BookType
+    {
+        public virtual long Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Authors { get; set; }
+        public virtual string Category { get; set; }
+        public virtual int Price { get; set; }
+        public virtual BookTypeDetails BookTypeDetails { get; set; }
+    }
+}
