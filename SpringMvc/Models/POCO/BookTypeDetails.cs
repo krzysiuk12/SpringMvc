edff62cdda4d2c4cb93cf6904d522735a158830b
Netoperek <piotrkala92@gmail.com>
2013-11-12 19:11:46 +0100

    BookTypeDetails - adding new class table to project

---
+++ SpringMvc/Models/POCO/BookTypeDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpringMvc.Models.POCO
+{
+    public class BookTypeDetails
+    {
+        public virtual long Id { get; set; }
+        public virtual int Quantity { get; set; }
+    }
+}
