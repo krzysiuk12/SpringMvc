0b73814e4c847bcbf37f731e9d95c3ef4a6fa72a
Jakub Krawczyk <pinkikvk@gmail.com>
2013-11-13 00:15:37 +0100

    [Suggestion] Add SuggestionService

---
+++ SpringMvc/Models/Suggestions/Services/Implementation/SuggestionService.cs
@@ -0,0 +1,33 @@
+﻿using SpringMvc.Models.Common;
+using SpringMvc.Models.POCO;
+using SpringMvc.Models.Suggestions.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpringMvc.Models.Suggestions.Services.Implementation
+{
+    public class SuggestionService : BaseSpringService, ISuggestionService
+    {
+        public IEnumerable<BookType> GetSuggestionsForUser(long userID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BookType> GetSuggestionsForUser(long userID, string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BookType> GetSuggestionsForGuest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BookType> GetSuggestionsForGuest(string category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
