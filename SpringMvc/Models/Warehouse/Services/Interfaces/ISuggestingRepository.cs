83ff54f4e4169fafce7fc926c6f5a27df5081371
Netoperek <piotrkala92@gmail.com>
2013-11-12 19:20:24 +0100

    Adding interface for Suggesting module

---
+++ SpringMvc/Models/Warehouse/Services/Interfaces/ISuggestingRepository.cs
@@ -0,0 +1,14 @@
+﻿using SpringMvc.Models.POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpringMvc.Models.Warehouse.Services.Interfaces
+{
+    interface ISuggestingRepository
+    {
+        List<BookType> GetAllBooks();
+        List<BookType> GetBooksByCategory(String category);
+    }
+}
