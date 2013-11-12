9bd971f8a387427afd5a144e9be8cde873362847
Netoperek <piotrkala92@gmail.com>
2013-11-12 19:19:33 +0100

    Adding interface for shop and sending module

---
+++ SpringMvc/Models/Warehouse/Services/Interfaces/ISending&ShopRepository.cs
@@ -0,0 +1,14 @@
+﻿using SpringMvc.Models.POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpringMvc.Models.Warehouse.Services.Interfaces
+{
+    interface ISending_ShopRepository
+    {
+        bool MarkSold(long BookTypeId, int quantity);
+        BookType GetBookTypeByID(long id);
+    }
+}
