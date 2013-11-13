00ec636517bb7e9848ae7cb6fe53eafb684a9b04
lexus00 <blazej1@interia.eu>
2013-11-13 11:44:53 +0100

    [Shop] init class Order

---
+++ SpringMvc/Models/POCO/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpringMvc.Models.POCO
+{
+    public class Order
+    {
+        enum OrderState { Ordered, InProgress, Paid, Finished };
+
+        public virtual long Id { get; set; }
+        public virtual long CustomerID { get; set; }
+        public virtual Dictionary<long, long> OrderEntries { get; set; }
+        public virtual DateTime SentDate { get; set; }
+        public virtual DateTime OrderDate { get; set; }
+        public virtual DateTime FinishedDate { get; set; }
+        public virtual OrderState status { get; set; }
+    }
+}
