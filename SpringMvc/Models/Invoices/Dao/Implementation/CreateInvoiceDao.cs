ff4e38fbb9e1a7b601d88c8780622e4a2e82a4ea
Taz <tomasz.kozubowski@gmail.com>
2013-11-14 20:10:30 +0100

    [Invoices] Models.Invoices.Dao: Added CreateInvoiceDao class and interface.

---
+++ SpringMvc/Models/Invoices/Dao/Implementation/CreateInvoiceDao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpringMvc.Models.Invoices.Dao.Implementation
+{
+    public class CreateInvoiceDao
+    {
+    }
+}
