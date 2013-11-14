e2639406df6fdda395149b1411b6bc3d214c1043
Netoperek <piotrkala92@gmail.com>
2013-11-14 13:36:42 +0100

    [Storehouse] Changing Fields of BookType

--- SpringMvc/Models/POCO/BookType.cs
+++ SpringMvc/Models/POCO/BookType.cs
@@ -8,8 +8,8 @@ namespace SpringMvc.Models.POCO
     public class BookType
     {
         public virtual long Id { get; set; }
-        public virtual long CategoryId { get; set; }
-        public virtual long QuantityMapId { get; set; }
+        public virtual Category Category { get; set; }
+        public virtual QuantityMap QuantityMap { get; set; }
         public virtual string Title { get; set; }
         public virtual string Authors { get; set; }
         public virtual int Price { get; set; }
