e9a933cfe1b685023f996a066ae8f5a4ba2aa34f
Taz <tomasz.kozubowski@gmail.com>
2013-11-15 00:55:50 +0100

    [Invoices] Models.Invoices.Services: Added Invoice builder class and interface (partially implemented).

---
+++ SpringMvc/Models/Invoices/Services/Implementation/PdfInvoiceBuilder.cs
@@ -0,0 +1,55 @@
+﻿using SpringMvc.Models.POCO;
+using SpringMvc.Models.Common;
+using SpringMvc.Models.Common.Interfaces;
+using SpringMvc.Models.Invoices.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Data;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace SpringMvc.Models.Invoices.Services.Implementation
+{
+    private class PdfInvoiceBuilder : BaseSpringService, IPdfInvoiceBuilder
+    {
+        protected void BuildInvoice(Order orderDetails)
+        {
+            try {
+
+                UserAccount userDetails = ServiceLocator.UserInformationService.GetUserAccountById(orderDetails.User.Id);
+
+                using (System.IO.FileStream fs = new FileStream(Server.MapPath("invoices") + "\\" 
+                    + orderDetails.Id.ToString() + DateTime.Now.ToString() + ".pdf", FileMode.Create))
+                    {
+                        Document document = new Document(PageSize.A4, 25, 25, 30, 1);
+                        PdfWriter writer = PdfWriter.GetInstance(document, fs);
+
+                        document.AddAuthor("BookStore");
+                        document.AddTitle("Customer Invoice - order id:" + orderDetails.Id.ToString());
+
+                        document.Open();
+
+                        PdfContentByte cb = writer.DirectContent;
+                        //cb.AddTemplate(PdfFooter(cb, drPayee), 30, 1);
+
+                        iTextSharp.text.Image png = iTextSharp.text.Image.GetInstance(Server.MapPath("logo.png"));
+                        png.ScaleAbsolute(200, 55);
+                        png.SetAbsolutePosition(40, 750);
+                        cb.AddImage(png);
+
+                        cb.BeginText();
+                    }
+            }
+            catch(Exception e)
+            {
+                
+            }
+        }
+    }
+}
---
+++ SpringMvc/Models/Invoices/Services/Interfaces/IPdfInvoiceBuilder.cs
@@ -0,0 +1,13 @@
+﻿using SpringMvc.Models.POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpringMvc.Models.Invoices.Services.Interfaces
+{
+    interface IPdfInvoiceBuilder
+    {
+        protected void BuildInvoice(Order orderDetails, UserAccount userDetails);
+    }
+}
