29b67276977a2ce6da84f1ad10129bc0a951fcbc
Slawex <slawomirbukowiec@gmail.com>
2013-11-14 18:40:51 +0100

    [shipment] added IShipmentService

---
+++ SpringMvc/Models/Shipment/Services/Interfaces/IShipmentService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpringMvc.Models.Shipment.Services.Interfaces
+{
+    interface IShipmentService
+    {
+    }
+}
