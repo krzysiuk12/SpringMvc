32340baa2a1000356a461051f7c21c93bd24bbd3
Taz <tomasz.kozubowski@gmail.com>
2013-11-14 18:49:22 +0100

    [Invoices] Test DAO method.

---
+++ SpringMvc/Models/Invoices/Dao/Interfaces/ITestDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpringMvc.Models.Invoices.Dao.Interfaces
+{
+    public interface ITestDao
+    {
+        public void TestMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
