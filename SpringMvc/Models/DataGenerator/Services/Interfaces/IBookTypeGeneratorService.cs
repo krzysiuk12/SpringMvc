cbf9924823a5671f3de6efd493c74e40145e8a6b
krzysiuk12 <kkrzysiu@poczta.onet.pl>
2013-11-15 19:00:56 +0100

    [Generator] BookTypeGenerator

--- SpringMvc/Models/DataGenerator/Services/Interfaces/IBookTypeGeneratorService.cs
+++ SpringMvc/Models/DataGenerator/Services/Interfaces/IBookTypeGeneratorService.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using SpringMvc.Models.POCO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace SpringMvc.Models.DataGenerator.Services.Interfaces
 {
-    interface IBookTypeGeneratorService
+    public interface IBookTypeGeneratorService
     {
+        List<Category> GenerateCategories();
+
+        List<QuantityMap> GenerateQuantityMaps();
+
+        List<BookType> GenerateBooks(List<Category> categories, List<QuantityMap> quantityMaps);
     }
 }
