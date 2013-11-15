307a590d901646a137aa1e697d78640cf5bf6ad8
Slawex <slawomirbukowiec@gmail.com>
2013-11-15 20:10:27 +0100

    [Shipment] Shipment Implementation and Inteface

---
+++ SpringMvc/Models/Shipment/Services/Implementation/ShipmentPreparationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpringMvc.Models.Shipment.Services.Implementation
+{
+    public class ShipmentPreparationService
+    {
+    }
+}
