7eadd18ea548448e4f5bd65da792152eb9e30d7d
Netoperek <piotrkala92@gmail.com>
2013-11-16 13:07:28 +0100

    [Storehouse] implementing IBooksInformationDao

--- SpringMvc/Models/Storehouse/Dao/Interfaces/IBooksInformationDao.cs
+++ SpringMvc/Models/Storehouse/Dao/Interfaces/IBooksInformationDao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SpringMvc.Models.POCO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +8,10 @@ namespace SpringMvc.Models.Storehouse.Dao.Interfaces
 {
     public interface IBooksInformationDao
     {
+        IEnumerable<BookType> GetBooksByCategory(long CategoryId);
+
+        IEnumerable<BookType> GetAllBooks();
+
+        BookType GetBookById(long BookTypeId);
     }
 }
