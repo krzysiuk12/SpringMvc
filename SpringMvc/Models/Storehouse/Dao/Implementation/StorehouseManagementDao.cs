2460a491f4bb1f80a1718e99aa95f05a23d30fef
krzysiuk12 <kkrzysiu@poczta.onet.pl>
2013-11-16 12:17:37 +0100

    [Storehouse] Interfaces

---
+++ SpringMvc/Models/Storehouse/Dao/Implementation/StorehouseManagementDao.cs
@@ -0,0 +1,13 @@
+﻿using SpringMvc.Models.Common;
+using SpringMvc.Models.Storehouse.Dao.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpringMvc.Models.Storehouse.Dao.Implementation
+{
+    public class StorehouseManagementDao : BaseHibernateDao, IStorehouseManagementDao
+    {
+    }
+}
---
+++ SpringMvc/Models/Storehouse/Services/Implementation/StorehouseManagementService.cs
@@ -0,0 +1,13 @@
+﻿using SpringMvc.Models.Common;
+using SpringMvc.Models.Storehouse.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpringMvc.Models.Storehouse.Services.Implementation
+{
+    public class StorehouseManagementService : BaseSpringService, IStorehouseManagementService
+    {
+    }
+}
