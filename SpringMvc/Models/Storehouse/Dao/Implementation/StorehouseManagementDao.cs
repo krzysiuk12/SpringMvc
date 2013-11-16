8067bc6f8f49e1e02b694d1279b67af818226de1
Netoperek <piotrkala92@gmail.com>
2013-11-16 13:07:45 +0100

    [Storehouse] implementing StorehouseManagementDao

--- SpringMvc/Models/Storehouse/Dao/Implementation/StorehouseManagementDao.cs
+++ SpringMvc/Models/Storehouse/Dao/Implementation/StorehouseManagementDao.cs
@@ -9,5 +9,10 @@ namespace SpringMvc.Models.Storehouse.Dao.Implementation
 {
     public class StorehouseManagementDao : BaseHibernateDao, IStorehouseManagementDao
     {
+        void AddCategory(String name);
+
+        void AddBookType(String title, String authors, long CategoryID, long QuantityMapId, decimal price);
+
+        void MarkSold(long id, int quantity);
     }
 }
