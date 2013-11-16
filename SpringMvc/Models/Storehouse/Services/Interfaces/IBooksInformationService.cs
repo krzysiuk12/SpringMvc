953148b6a52d958f1bcd44b78a1ab6ba7da5a05b
Netoperek <piotrkala92@gmail.com>
2013-11-16 13:05:49 +0100

    [Storehouse] implementing IBooksInformationService

--- SpringMvc/Models/Storehouse/Services/Interfaces/IBooksInformationService.cs
+++ SpringMvc/Models/Storehouse/Services/Interfaces/IBooksInformationService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SpringMvc.Models.POCO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +8,10 @@ namespace SpringMvc.Models.Storehouse.Services.Interfaces
 {
     public interface IBooksInformationService
     {
+        IEnumerable<BookType> GetBooksByCategory(long CategoryId);
+
+        IEnumerable<BookType> GetAllBooks();
+
+        BookType GetBookById(long BookTypeId);
     }
 }
