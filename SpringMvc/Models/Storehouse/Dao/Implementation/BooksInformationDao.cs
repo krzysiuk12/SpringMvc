92e54bf1c671d826ff13ac9be0da1d29579abfe9
Netoperek <piotrkala92@gmail.com>
2013-11-16 13:08:04 +0100

    [Storehouse] implementing BooksInformationDao

--- SpringMvc/Models/Storehouse/Dao/Implementation/BooksInformationDao.cs
+++ SpringMvc/Models/Storehouse/Dao/Implementation/BooksInformationDao.cs
@@ -1,13 +1,31 @@
 ﻿using SpringMvc.Models.Common;
+using SpringMvc.Models.POCO;
 using SpringMvc.Models.Storehouse.Dao.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using NHibernate.Linq;
+
 
 namespace SpringMvc.Models.Storehouse.Dao.Implementation
 {
+
     public class BooksInformationDao : BaseHibernateDao, IBooksInformationDao
     {
+        IEnumerable<BookType> GetBooksByCategory(long CategoryId)
+        {
+            return this.Session.Query<BookType>().Where(bookType => bookType.Category.Id == CategoryId).Select(bookType => bookType).ToList();
+        }
+
+        public IEnumerable<BookType> GetAllBooks()
+        {
+            return this.Session.Query<BookType>().Where(bookType => bookType.Id != -1).Select(bookType => bookType).ToList();
+        }
+
+        public BookType GetBookById(long BookTypeId)
+        {
+            return this.Session.Query<BookType>().Where(bookType => bookType.Id == BookTypeId).Select(bookType => bookType).Single();
+        }
     }
 }
