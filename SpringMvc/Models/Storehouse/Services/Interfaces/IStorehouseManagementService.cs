d3b37aab65d9832ddaab583b308b2106c5bf52da
Netoperek <piotrkala92@gmail.com>
2013-11-16 13:05:26 +0100

    [Storehouse] implementing IStorehouseManagamentService

--- SpringMvc/Models/Storehouse/Services/Interfaces/IStorehouseManagementService.cs
+++ SpringMvc/Models/Storehouse/Services/Interfaces/IStorehouseManagementService.cs
@@ -7,5 +7,10 @@ namespace SpringMvc.Models.Storehouse.Services.Interfaces
 {
     public interface IStorehouseManagementService
     {
+        void AddCategory(String name);
+
+        void AddBookType(String title, String authors, long CategoryID, long QuantityMapId,decimal price);
+
+        void MarkSold(long id, int quantity);
     }
 }
