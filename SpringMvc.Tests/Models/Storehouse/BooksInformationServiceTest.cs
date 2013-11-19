3a0574b40619f8c9725849b35e1208da0b057e53
mttziel <mttziel@gmail.com>
2013-11-19 23:37:47 +0100

    [Test]  BooksInformationServiceTest

---
+++ SpringMvc.Tests/Models/Storehouse/BooksInformationServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpringMvc.Models.POCO;
+using SpringMvc.Models.Storehouse.Services.Implementation;
+
+namespace SpringMvc.Tests.Models.Storehouse
+{
+    [TestClass]
+    public class BooksInformationServiceTest
+    {
+        private BooksInformationService bis = new BooksInformationService();
+        [TestMethod]
+        public void TestGetAllCategories()
+        {
+            IList<Category> list = bis.GetAllCategories();
+            Assert.AreEqual(10, list.Count);
+        }
+        [TestMethod]
+        public void TestGetBookTypeById()
+        {
+            BookType book = bis.GetBookTypeById(-1);
+            Assert.IsNull(book);
+        }
+        [TestMethod]
+        public void TestGetAllBooks()
+        {
+            IEnumerable<BookType> list = bis.GetAllBooks();
+            Assert.AreEqual(100, (new LinkedList<BookType> (list)).Count);
+        }
+        [TestMethod]
+        public void TestGetBooksByCategoryId()
+        {
+            IEnumerable<BookType> list = bis.GetBooksByCategoryId(-1);
+            Assert.AreEqual(0, (new LinkedList<BookType>(list)).Count);
+        }
+    }
+}
