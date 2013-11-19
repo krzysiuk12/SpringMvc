638e34172e7f668fa2d91ef1f4fbaa9cc3bceb07
Slawex <slawomirbukowiec@gmail.com>
2013-11-20 00:11:48 +0100

    [Shipment, controllers] Modified ShipmentController, added OrderDetailsController

---
+++ SpringMvc/Controllers/OrderDetailsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using SpringMvc.Models.Common.Interfaces;
+using SpringMvc.Models.POCO;
+
+namespace SpringMvc.Controllers
+{
+    public class OrderDetailsController : Controller
+    {
+        private IServiceLocator ServiceLocator { get; set; }
+
+        public ActionResult OrderDetails ()
+        {
+            Order order = ServiceLocator.OrderInformationsService.GetOrderById(1);
+            return View();
+        }
+    }
+}
