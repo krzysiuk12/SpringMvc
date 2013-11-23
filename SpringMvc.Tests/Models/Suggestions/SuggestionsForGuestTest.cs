f7f50fb13b3cb35d44a8ed1c9844c7c4f528faef
Jakub Krawczyk <pinkikvk@gmail.com>
2013-11-23 18:30:00 +0100

    [Test] Add mocks for SuggestionService tests

--- SpringMvc.Tests/Models/Suggestions/SuggestionsForGuestTest.cs
+++ SpringMvc.Tests/Models/Suggestions/SuggestionsForGuestTest.cs
@@ -3,8 +3,9 @@ using System.Linq;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SpringMvc.Models.POCO;
-using SpringMvc.Models.Suggestions.Services.Interfaces;
 using SpringMvc.Models.Suggestions.Services.Implementation;
+using SpringMvc.Models.Storehouse.Services.Interfaces;
+using NMock;
 
 
 namespace SpringMvc.Tests.Models.Suggestions
@@ -12,48 +13,69 @@ namespace SpringMvc.Tests.Models.Suggestions
     [TestClass]
     public class SuggestionsForGuestTest
     {
+        private static MockFactory _factory = new MockFactory();
 
-        private ISuggestionService suggestionService;
+        [ClassInitialize]
+        public static void TestsSetup(TestContext context)
+        {
+            var mockBookInformationService = _factory.CreateMock<IBooksInformationService>();
+            BookType book = new BookType();
+            List<BookType> bookList = new List<BookType>();
+            for (int i = 0; i < 10; i++)
+            {
+                BookType tempBook = new BookType();
+                tempBook.Id = 0;
+            }
+
+            mockBookInformationService.Expects.Any.Method(_ => _.GetAllBooks()).WillReturn(bookList);
+            mockBookInformationService.Expects.Any.Method(_ => _.GetBooksByCategoryId(0)).WithAnyArguments().WillReturn(bookList);
+            mockBookInformationService.Expects.Any.Method(_ => _.GetBookTypeById(0)).WithAnyArguments().WillReturn(book);
+
+            //TODO!!
+            //serviceLocator.BooksInformationService = mockBookInformationService
+        }
 
         [TestInitialize]
         public void Initialize()
         {
-            suggestionService = new SuggestionService();
+            //TODO!!
+            //AplicationScope.GlobalSuggestionCache = null
         }
 
-        [TestMethod]
-        public void ResultQuantityTest()
+        [TestCleanup]
+        public void Cleanup()
         {
-            IEnumerable<BookType> result = suggestionService.GetSuggestionsForGuest();
-
-            Int32 counter = 0;
-            foreach (BookType book in result)
-            {
-                Assert.IsNotNull(book);
-                counter++;
-            }
-
-            Assert.Equals(counter, 5);
+            _factory.VerifyAllExpectationsHaveBeenMet();
+            _factory.ClearExpectations();
         }
 
         [TestMethod]
         public void NoCacheUpdateTest()
         {
-            List<BookType> result1 = suggestionService.GetSuggestionsForGuest().ToList();
-            List<BookType> result2 = suggestionService.GetSuggestionsForGuest().ToList();
+            var mockSuggestionCache = _factory.CreateMock<SuggestionCache>();
+            mockSuggestionCache.Expects.One.GetProperty(_ => _.GenerationTime).WillReturn(DateTime.Now);
+            List<long> bookList = new List<long>();
+            mockSuggestionCache.Expects.One.GetProperty(_ => _.BookList).WillReturn(bookList);
 
-            Assert.Equals(result1.Count, result2.Count);
+            //TODO!!
+            //AplicationScope.GlobalSuggestionCache = mockSuggestionCache
+        }
 
-            foreach (BookType book in result1)
-            {
-                Assert.IsTrue(result2.Contains(book));
-            }
+        [TestMethod]
+        public void ResultQuantityTest()
+        {
+            IEnumerable<BookType> result = null;
+            //TODO!!
+            // result = suggestionService.GetSuggestionsForGuest();
+            Assert.Equals(result.Count(), 5);
         }
 
         [TestMethod]
         public void DistinctBookTest()
         {
-            IEnumerable<BookType> result = suggestionService.GetSuggestionsForGuest();
+            IEnumerable<BookType> result = null;
+            //TODO!!
+            // result = suggestionService.GetSuggestionsForGuest();
             Assert.Equals(result.Count(), result.Distinct().Count());
         }
     }
