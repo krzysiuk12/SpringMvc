b67e534b94fa219f689271f97e03c8aa74e3ce83
mttziel <mttziel@gmail.com>
2013-11-23 10:37:32 +0100

    [Tests] Added tests for OrderInformationService

--- SpringMvc.Tests/Models/Shop/OrderInformationsServiceTest.cs
+++ SpringMvc.Tests/Models/Shop/OrderInformationsServiceTest.cs
@@ -4,18 +4,63 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SpringMvc.Models.POCO;
 using SpringMvc.Models.Shop.Services.Implementation;
 using SpringMvc.Models.Shop.Services.Interfaces;
-
+using SpringMvc.Models.UserAccounts.Services.Implementation;
+using SpringMvc.Models.UserAccounts.Services.Interfaces;
 namespace SpringMvc.Tests.Models.Shop
 {
     [TestClass]
     public class OrderInformationsServiceTest
     {
         private IOrderInformationsService ois = new OrderInformationsService();
+        private IOrderManagementService oms = new OrderManagementService();
+        private IAccountAdministrationService aas = new AccountAdministrationService();
+        private Order order;
+        private UserAccount userAcc;
+
+        [ClassInitialize]
+        public void Class_Initialize()
+        {
+            userAcc = new UserAccount();
+            aas.SaveOrUpdateUser(userAcc);
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            order = new Order();
+            order.OrderEntries = null;
+            order.SentDate = DateTime.Now;
+            order.User = null;
+        }
+
+
+
         [TestMethod]
-        [ExpectedException(typeof(Exception))]
+        public void TestGetOrderById()
+        {
+            oms.CreateNewOrder(order);
+            Order testedOrder = ois.GetOrderById(order.Id);
+            Assert.AreEqual(testedOrder.Id, order.Id);
+        }
+
+        [TestMethod]
         public void TestGetOrderByIdWithWrongId()
         {
-            ois.GetOrderById(-1);
+            Order testedOrder = ois.GetOrderById(-1);
+            Assert.IsNull(testedOrder);
+        }
+
+        [TestMethod]
+        public void TestGetOrdersByUserId()
+        {
+            order.User = userAcc;
+            oms.CreateNewOrder(order);
+            bool isOrderThere = false;
+            foreach (var item in ois.GetOrdersByUserId(userAcc.Id))
+            {
+                if (item.Id == order.Id) isOrderThere = true;
+            }
+            Assert.IsTrue(isOrderThere);
         }
 
         [TestMethod]
@@ -26,20 +71,46 @@ namespace SpringMvc.Tests.Models.Shop
         }
 
         [TestMethod]
-        public void TestGetInProgressOrdersByUserIdWithWrongId()
+        public void TestGetUndeliveredOrders()
         {
-            IEnumerable<Order> orders = ois.GetUndeliveredOrdersByUserId(-1);
-            Assert.Equals((new LinkedList<Order>(orders)).Count, 0);
+            order.Status = Order.OrderState.ORDERED;
+            oms.CreateNewOrder(order);
+            bool isOrderThere = false;
+            foreach (var item in ois.GetUndeliveredOrders())
+            {
+                if (item.Id == order.Id) isOrderThere = true;
+            }
+            Assert.IsTrue(isOrderThere);
         }
 
         [TestMethod]
-        public void TestGetInProgressOrders()
+        public void TestGetUndeliveredOrdersByUserId()
         {
-            IEnumerable<Order> orders = ois.GetUndeliveredOrders();
-            foreach (var item in orders)
+            order.User = userAcc;
+            order.Status = Order.OrderState.ORDERED;
+            oms.CreateNewOrder(order);
+            bool isOrderThere = false;
+            foreach (var item in ois.GetUndeliveredOrdersByUserId(userAcc.Id))
             {
-                Assert.IsNotNull(item);
+                Assert.AreEqual(item.User.Id, userAcc.Id);
+                if (item.Id == order.Id) isOrderThere = true;
             }
+            Assert.IsTrue(isOrderThere);
+        }
+
+        [TestMethod]
+        public void TestGetDeliveredOrdersByUserId()
+        {
+            order.User = userAcc;
+            order.Status = Order.OrderState.DELIVERED;
+            oms.CreateNewOrder(order);
+            bool isOrderThere = false;
+            foreach (var item in ois.GetDeliveredOrdersByUserId(userAcc.Id))
+            {
+                Assert.AreEqual(item.User.Id, userAcc.Id);
+                if (item.Id == order.Id) isOrderThere = true;
+            }
+            Assert.IsTrue(isOrderThere);
         }
     }
 }
